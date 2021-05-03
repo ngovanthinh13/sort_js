34a274bf386454b616023ace9972ac97e3876e95
ngovanthinh13 <luonchomotdieu@gmail.com>
2021-05-04 00:28:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+//   $('#example').DataTable( {
+//     "ajax": 'data.json'
+// } );
+  // $.getJSON("data.json", function(products){
+  //           console.log(products.data); // Prints: Harry
+  //           // console.log(data.type); // Prints: 14
+  //       }).fail(function(){
+  //           console.log("An error has occurred.");
+  //       });	
+
+
+  // $("p").click(function(){
+  //   $(this).hide();
+  // });
+});
